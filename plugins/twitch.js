333ebb77f91ea4a280980cd566dc8b971b9c4643
dogr95 <rayquasa181@gmail.com>
2021-02-18 11:25:49 +0100

    a

--- plugins/twitch.js
+++ plugins/twitch.js
@@ -238,7 +238,7 @@ const cors = require('cors');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors({
-  origin: "*",
+  origin: "https://alleshusos.de/",
   optionsSuccessStatus: 200,
   credentials: true
 }))
