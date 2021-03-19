231d0ffdae64f70c9d9c9f2479d7eae0a0fbfadc
dogr95 <rayquasa181@gmail.com>
2021-03-19 16:27:56 +0100

    ~

--- plugins/discord/commands/skip.js
+++ plugins/discord/commands/skip.js
@@ -1,13 +1,16 @@
 module.exports = function(client, message, args) {
   const mediaPlayer = client[message.guild.id]
-  // check if user entered amount of skipped songs
-  if(!Number.isNaN(parseInt(args[0])) && parseInt(args[0] > 0)) {
-    args[0] = parseInt(args[0])
-  } else {
-    // otherwise set it to 1
+
+  args[0] = parseInt(args[0])
+
+  if(Number.isNaN(args[0]) || args[0] < 0) {
     args[0] = 1
   }
+
+  console.log(args[0])
+  
   for(let i = 0; i < args[0]; i++) {
+    console.log("skipped. "+i)
     mediaPlayer.skip()
   }
 }
